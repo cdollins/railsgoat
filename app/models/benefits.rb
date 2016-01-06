ce2b3dda9d98a71c6198ae6c663f8e2b76cc9ab5
Chad Dollins <chad.dollins@livingsocial.com>
2016-01-05 21:05:43 -0600

    Fix fir CLI exploit in beneifts model

--- app/models/benefits.rb
+++ app/models/benefits.rb
@@ -11,9 +11,7 @@ class Benefits < ActiveRecord::Base
   end
 
   def self.make_backup(file, data_path, full_file_name)
-    if File.exists?(full_file_name)
-      silence_streams(STDERR) { system("cp #{full_file_name} #{data_path}/bak#{Time.zone.now.to_i}_#{file.original_filename}") }
-    end
+    FileUtils.cp "#{full_file_name}", "#{data_path}/bak#{Time.zone.now.to_i}_#{file.original_filename}"
   end
 
 =begin
